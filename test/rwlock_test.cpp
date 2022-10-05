9492d82b261f303351513b172e874266c8f13bd8
hoptimized <57064437+hoptimized@users.noreply.github.com>
2022-10-05 15:38:42 +0200

    Revert "Move pthreads definition into lib makefile"
    This reverts commit 5c695b99cfab56d210d7909053e65eecd7577160.
    
    Revert "Add some debug info"
    
    This reverts commit b3e92780266477578865560b46526ae9a01ef20c.

--- test/rwlock_test.cpp
+++ test/rwlock_test.cpp
@@ -99,8 +99,7 @@ SCENARIO("Can only acquire one concurrent write lock")
             }
             AND_THEN("The second lockguard has not been acquired")
             {
-                INFO("lock2.has_value() = " << lock2.has_value());
-                REQUIRE(!lock2.has_value()); // FAILS
+                REQUIRE(!lock2.has_value());
             }
         }
     }
@@ -125,8 +124,7 @@ SCENARIO("Cannot acquire read lock when write lock exists")
             }
             AND_THEN("The read-lockguard has not been acquired")
             {
-                INFO("lock2.has_value() = " << lock2.has_value());
-                REQUIRE(!lock2.has_value()); // FAILS
+                REQUIRE(!lock2.has_value());
             }
         }
     }
